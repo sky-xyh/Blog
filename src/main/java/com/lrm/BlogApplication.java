e1b8fe8d7fe59388a828e3f68982f914037f0b17
xyh <729301487@qq.com>
2020-10-13 21:18:15 +0800

    博客源码

---
+++ src/main/java/com/lrm/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.lrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+}
